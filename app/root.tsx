803655711251b66a3a64935681a3675e5ac217a9
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-05-06 16:08:19 +0530

    setup syncfusion

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { registerLicense } from "@syncfusion/ej2-base";
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
